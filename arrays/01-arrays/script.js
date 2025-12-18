3005593ec63e86de251d7077dbbad685a87ceb0c
GabrielaTumfart <gabrielatumfart@gmail.com>
2025-12-18 16:03:15 +0100

    cerco l'errore

--- arrays/01-arrays/script.js
+++ arrays/01-arrays/script.js
@@ -25,7 +25,7 @@ console.table(teachers);
 
 const fourthTeacher = teachers[3];
 
-outputColor = background = rgb (${outputR}; ${outputG}; ${outputB}; ) //*mi ha insegnato tiziano oggi nella lezione
+outputColor = background = rgb(${outputR}; ${outputG}; ${outputB}; ) //*mi ha insegnato tiziano oggi nella lezione
 
 console.log("L'insegnante della quarta posizione è:", % C + , fourthTeacher, outputColor);
 
