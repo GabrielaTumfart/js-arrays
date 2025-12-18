4ef797ed9d15db415b5b46aaeff029567e301806
GabrielaTumfart <gabrielatumfart@gmail.com>
2025-12-18 16:15:41 +0100

    lascio da parte e inizio l'esercizio 2

--- arrays/01-arrays/script.js
+++ arrays/01-arrays/script.js
@@ -26,14 +26,16 @@ console.table(teachers);
 const fourthTeacher = teachers[3];
 
 //*mi ha insegnato tiziano oggi nella lezione
-let outputR = 0
+/*let outputR = 0
 let outputG = 0
 let outputB = 0
 let outputColor = 0
 
 outputColor = background rgb (${outputR}; ${outputG}; ${outputB});  
 
-console.log("L'insegnante della quarta posizione è:", % C + , fourthTeacher, outputColor);
+console.log("L'insegnante della quarta posizione è:", % C + , fourthTeacher, outputColor); */
+
+console.log("L'insegnante della quarta posizione è:",fourthTeacher); 
 
 
 
