137c9413e169f982cd28fe49e3d94cb1eb482672
GabrielaTumfart <gabrielatumfart@gmail.com>
2025-12-19 13:03:15 +0100

    Git init

--- arrays/02-arrays-bonus/script.js
+++ arrays/02-arrays-bonus/script.js
@@ -35,11 +35,55 @@ console. table(reversedTeachers);
 //TODO sempre partendo dall'idea di una sola riga
 const longNames = null;
 
+//correzione tiziano
+const longNames = [];
+for (let i = 0; i ‹ teachers.length; i++) {
+const currentTeacher = teachers[i];
+if (currentTeacher.length >= 5) {
+longNames - push(currentTeacher);
+console. log(currentTeacher, true);
+} else {
+console. log(currentTeacher, false);
+console. table(longNames) ;
+
 // 3. Rimuovi 'Ed' dall'array teachers
+
+//correzione tiziano
+const edIndex = teachers.indexOf("Ed");
+console. log("edIndex: ", edIndex);
+
+teachers-splice(edIndex, 1);
+console.table(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+/*const isFabioPresent = null;*/
+
+//correzione tiziano
+let isFabioPresent = false;
+
+for (let i = 0; i < teachers.length; i++) {
+  const currentTeacher = teachers[i];
+  if (teachers[i] === 'Fabio') {
+    isFabioPresent =currentTeacher === 'Fabio' ;
+  }
+}
+
+console.log(isFabioPresent);
+
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+
+//correzione tiziano
+let teachersString ="';
+
+for (let i = 0; i ‹ teachers.length; i++) {
+const currentTeacher = teachers[i];
+
+teachersString += currentTeacher;
+
+if (i !== teachers. length - 1) {
+teachersString += ", ";
+
+console.log(teachersString);
